5336b9394ad57d8ccd238861f3147fa6550f72a1
Vinu <vinu@Vinus-MacBook-Air.local>
2021-12-10 23:01:57 +0530

    19 Questions Done

---
+++ Chapter-2 Functions.py
@@ -0,0 +1,198 @@
+# Questions - 2
+
+# print(type(type(int)))
+
+
+
+
+
+
+
+# Questions - 3
+
+# print(len(['hello',2,4,6]))
+
+
+
+
+
+
+# Questions - 4
+
+# import math
+# print(abs(math.sqrt(25)))
+
+
+
+
+
+# Questions - 5
+
+# print(min(max(False, -13, -4),2,7))
+
+
+
+
+
+# Questions - 11
+
+# def sum():
+#     calculate()
+# def calculate():
+#     x = int(14.5)
+#     y = float(24.6)
+#     s = x + y
+#     print('%d' %s)
+# sum()
+
+
+
+
+
+
+# Questions - 12
+
+# def addition(j):
+#     if (j >= 4):
+#         j = j * j
+#     else:
+#         j = j * 2
+#     return j
+# i = 4
+# a = addition(i)
+# print('%d' %a)
+
+
+
+
+
+
+
+
+
+
+# Questions - 13
+
+# def fun(b):
+#     x = b * b
+#     return x
+# for i in range(1, 6):
+#     a = fun(i)
+# print('%d' %a, end=" ")
+
+
+
+
+
+
+
+
+# Questions - 14
+
+# def area(a):
+#     return (a*a)
+# def area1(l, b):
+#     return (l*b)
+# a1 = area(4)
+# print('%d' %a1)
+# a2 = area1(5,6)
+# print('%d' %a2)
+
+
+
+
+
+
+
+
+# Questions - 15
+
+# i = 100
+# def abc():
+#     i = 10
+#     print('first = %d' %i)
+# def main():
+#     i = 2
+#     abc()
+#     print('second = %d' %i)
+# main()
+
+
+
+
+
+
+
+
+
+# Questions - 16
+
+# a = 3
+# def fun(x,y,z):
+#     a = 3
+#     a = a + (x + y)
+#     z = a + y
+#     y = y + x
+# a = 2
+# b = 5
+# fun(a,a,b)
+# print('%d %d %d' %(a, a, b))
+
+
+
+
+
+
+# Questions - 17
+
+# def max(x, y, z):
+#     if(x > y and y > z):
+#         y = y + 1
+#         z = z + 1
+#         return x
+#     else:
+#         if (y > x):
+#             return y
+#         else:
+#             return z
+# a,b,c = 10,13,8
+# a = max(a,b,c)
+# print('%d %d %d' %(a,b,c))
+# b = max(a,b,c)
+# print('%d %d %d' %(a+1,b+1,c+1))
+# c = max(a,b,c)
+# print('%d %d %d' %(a-1,b-1,c-1))
+
+
+
+
+
+
+
+# Questions - 18
+
+# def calc(u):
+#     if (u % 2 == 0):
+#         return u + 10
+#     else:
+#         return u * 2
+# def pattern(m, b=2):
+#     for CNT in range(0, b):
+#         print(m*(calc(CNT)))
+# pattern('A', 2)
+
+
+
+
+
+
+
+
+
+
+
+# Questions - 19
+
+def cubic(x):
+    return x*x*x
+print(cubic(5))
